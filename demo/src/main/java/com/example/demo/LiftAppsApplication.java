512351ba2d059b33373c8bf040db897aced71d6c
knaveenkumar836 <106507636+knaveenkumar836@users.noreply.github.com>
2022-09-27 18:00:10 +0530

    initial commit

---
+++ demo/src/main/java/com/example/demo/LiftAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftAppsApplication.class, args);
+		System.out.println("Successed");	}
+
+}
---
+++ demo/src/main/java/com/example/demo/pojo/ElevatorDirection.java
@@ -0,0 +1,7 @@
+package com.example.demo.pojo;
+
+public enum ElevatorDirection {
+	  ELEVATOR_UP,
+	  ELEVATOR_DOWN,
+	  ELEVATOR_HOLD
+	}
